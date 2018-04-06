4a97d2dfa010c0ed30a2fca8e9b1a6fc9e937b05
kleberfauzer <kleberfauzer42@gmail.com>
2018-04-06 16:42:25 -0300

    cad_cliente

--- cad_cliente.php
+++ cad_cliente.php
@@ -2,13 +2,14 @@
 
 <html>
 	<body>
-		<a href="cad_conta.php">Cadastrar Conta</a> | 
-		<a href="list_cad.php">Listar Cadastro</a> | 
+		<a href="cad_cliente.php">Cadastrar Conta</a> | 
 		<a href="dep.php">Depósito</a> | 
 		<a href="saq.php">Saque</a> |
+		
 		<fieldset>
 			<legend>Cadastrar Conta</legend>
 		<?php 
+		if((file_exists("cliente.xml"))){
 		
 		session_start();
 		if(isset($_POST["nome_cnt"])){
@@ -20,8 +21,8 @@
 					}
 					$_SESSION["saldo"][$_SESSION["contador1"]] = 0;
 					$_SESSION["nome_cnt"][$_SESSION["contador1"]] = $_POST["nome_cnt"];
-					$_SESSION["cpf"][$_SESSION["contador1"]] = $_POST["cpf"];
-					$_SESSION["ag"][$_SESSION["contador1"]] = $_POST["ag"];
+					$_SESSION["email"][$_SESSION["contador1"]] = $_POST["email"];
+					$_SESSION["senha"][$_SESSION["contador1"]] = $_POST["senha"];
 					$_SESSION["contador1"]++;
 		?>	
 			
@@ -31,23 +32,40 @@
 		?>
 		
 		
-				<form method="post" action="cad_conta.php">
+				<form method="post" action="cad_cliente.php">
 					<label>Nome: </label>
 					<input type="text" name="nome_cnt" required="required"> <br>
-					<label>CPF: </label>
-					<input type="number" name="cpf" min="1" step="1" required="required"> <br>
-					<label>Agência: </label>
-					<select name="ag" required="required">
-						<option>---Selecione---</option>
-						<option>3010-4</option>
-						<option>1020-5</option>
-						<option>1241-3</option>
-						<option>1234-5</option>
-					</select><br>
+					<label>email: </label>
+					<input type="email" name="email" min="1" step="1" required="required"> <br>
+					<label>senha: </label>
+					<input type="password" name="senha" min="1" step="1" required="required"> <br>
+					
 					<input type = "submit" value = "enviar" />
 				</form>
 		</fieldset>
+		
+		
 		<?php
+		if((file_exists("cliente.xml"))){
+		
+		$arquivo = 'cliente.xml';
+					
+					$xml= simplexml_load_file($arquivo);
+					
+					$posicao = sizeof($xml -> dados);
+					if(isset($_POST["nome_cnt"])){
+			
+					
+					
+
+					 $xml->dados[$posicao ]->nome_cliente = "".$_POST["nome_cnt"];
+					 $xml->dados[$posicao ]->email = "".$_POST["email"];
+					 $xml->dados[$posicao ]->senha = "".$_POST["senha"];
+					
+					$xml -> asXML($arquivo);
+			
+			
+					}
 		}
 		?>
 	</body>
